e0b910e84849f3b6a493187319484d41c65f7303
codesver <codesver@gmail.com>
2024-10-10 18:01:02 +0900

    feat : move command to domain logic inside command

--- src/main/java/codesver/springboot/domain/member/application/service/MemberRestService.java
+++ src/main/java/codesver/springboot/domain/member/application/service/MemberRestService.java
@@ -3,7 +3,6 @@ package codesver.springboot.domain.member.application.service;
 import codesver.springboot.domain.member.application.model.command.MemberSearchCommand;
 import codesver.springboot.domain.member.application.repository.MemberRepository;
 import codesver.springboot.domain.member.domain.Members;
-import codesver.springboot.domain.member.domain.SearchMembers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,11 +16,6 @@ public class MemberRestService implements MemberService {
     @Override
     @Transactional(readOnly = true)
     public Members getMembers(MemberSearchCommand searchCommand) {
-        SearchMembers searchMembers = SearchMembers.builder()
-                .memberIds(searchCommand.getMemberIds())
-                .usernames(searchCommand.getUsernames())
-                .emails(searchCommand.getEmails())
-                .build();
-        return memberRepository.searchMembers(searchMembers);
+        return memberRepository.searchMembers(searchCommand.toDomain());
     }
 }
